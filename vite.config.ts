244bd36962a3f0716bac931932a5ed244e0e0e62
erichandronico <erich.munoz@gmail.com>
2025-08-30 12:49:56 -0400

    standard root

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   return {
-    plugins: [react(), tailwindcss()],
-    base: command === 'build' ? '/g3n-salud/' : '/', // 👈 usa '/' en dev, '/g3n-salud/' en build
+    plugins: [react(), tailwindcss()]
   }
 })
